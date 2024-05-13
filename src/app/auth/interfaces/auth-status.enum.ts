f997b803195f9763cc571c66757266810a546aea
Daniel Esteban <decepeda@misena.edu.co>
2024-05-13 16:53:22 -0500

    Se crean interfaces y se empieza a implementar el servicio de autenticacion

---
+++ src/app/auth/interfaces/auth-status.enum.ts
@@ -0,0 +1,6 @@
+
+export enum AuthStatus {
+    checking = 'checking',
+    authenticated = 'authenticated',
+    notauthenticated = 'notAuthenticated',
+}
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { enviroment } from '../../../enviroments/enviroments';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = enviroment.baseUrl;
+  private http = inject( HttpClient );
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking );
+
   constructor() { }
+
+  login( email: string, password: string ): Observable<boolean> {
+
+    return of(true);
+  }
 }
