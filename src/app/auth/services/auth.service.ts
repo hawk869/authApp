dccf0791c2d2b99896240dfed8df1511e8214e92
Daniel Esteban <decepeda@misena.edu.co>
2024-05-24 17:54:32 -0500

    Se implementa metodo logout

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -49,7 +49,10 @@ export class AuthService {
     const url = `${ this.baseUrl }/auth/check-token`;
     const token = localStorage.getItem('token');
 
-    if ( !token ) return of(false);
+    if ( !token ) {
+      this.logout();
+      return of(false);
+    }
 
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${ token }`);
@@ -62,5 +65,10 @@ export class AuthService {
             return of(false);
           })
         )
+  }
+  logout() {
+    localStorage.removeItem('token');
+    this._currentUser.set( null );
+    this._authStatus.set( AuthStatus.notauthenticated );
   }
 }
